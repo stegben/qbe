8c77e26b45f14ab85f619125d9f908766b2dcef3
stegben <stegben.benjamin@gmail.com>
2020-03-07 15:04:44 +0800

    ckpt 3/7

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from dotenv import
+from dotenv import load_dotenv
+import torch
+import torchaudio
 
 
 if __name__ == "__main__":
+
     # import audio data
     # split data into query and candidates
