47a3a423cf04c3dd2cc05aa1a716a8aaa42164f6
stegben <stegben.benjamin@gmail.com>
2020-05-25 23:12:59 +0800

    add more types

--- aqbe/types.py
+++ aqbe/types.py
@@ -4,10 +4,19 @@ import numpy as np
 from nptyping import NDArray
 
 
-AudioKey = int
+FrameID = int
+AudioKey = str
 Score = float
 Second = float
+
+VoiceType = NDArray[(Any,), np.float32]  # single channel, -1 ~ 1, length == sr * secs
+LoadedVoiceType = Tuple[VoiceType, int]  # Voice, sampleing rate
+
 AudioFeatureType = NDArray[(Any, Any), np.float32]
 FrameIdxType = NDArray[(Any,), np.int64]
+
+IndexQueryResult = List[Tuple[Score, FrameID, FrameID]]  # score, start frame id, end frame id
 CandidateType = Tuple[Score, AudioKey, Second, Second]  # score, Audio key, start second, end second
 QueryResult = List[CandidateType]
+
+AlignmentType = Tuple[str, AudioKey, Second, Second]  # label, Audio key, start_second, end_second
