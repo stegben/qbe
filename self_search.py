a3cd2cbe9cd1663d0f0b1c0531b2e26f483a8868
stegben <stegben.benjamin@gmail.com>
2020-06-02 22:50:04 +0800

    calculate score for self search

--- self_search.py
+++ self_search.py
@@ -2,11 +2,14 @@ import argparse
 import os
 
 from dotenv import load_dotenv
+import numpy as np
+from tqdm import tqdm
 
 from aqbe.data import LibriSpeechWithAlignment, Data
 from aqbe.audio_loader import TorchAudio
 from aqbe.encoder import MFCC
 from aqbe.index import SimpleRails
+from aqbe.eval import overlap_ratio
 
 
 def self_search_argparse():
@@ -19,6 +22,7 @@ def self_search_argparse():
     )
     parser.add_argument
     return parser
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -43,15 +47,36 @@ if __name__ == '__main__':
     )
     results = []
     labels = []
-    for _ in range(10000):
+    correct = []
+    query_length = 1.5
+    for _ in tqdm(range(100)):
         try:
-            key, start_sec, feature = data.sample_range(length=1.5)
+            key, start_sec, feature = data.sample_range(length=query_length)
         except:
-            import ipdb; ipdb.set_trace()
+            continue
+        try:
+            score, start_frame_idx, end_frame_idx = index.query(feature)[0]
+        except:
+            continue
         labels.append((key, start_sec))
-        try:
-            results.append(index.query(feature))
-        except:
-            import ipdb; ipdb.set_trace()
+        results.append((score, start_frame_idx, end_frame_idx))
 
+        query_result = data.reverse_lookup(start_frame_idx, end_frame_idx)
+        if len(query_result) > 2:
+            correct.append(0.)
+        elif key != query_result[0][0]:
+            correct.append(0.)
+        else:
+            query_start_sec = query_result[0][1]
+            query_end_sec = query_result[1][1]
+
+            score = overlap_ratio(
+                start_sec, start_sec + query_length,
+                query_start_sec, query_end_sec,
+            )
+            if score > 0.8:
+                correct.append(1)
+            else:
+                correct.append(0)
+    print(f'score: {np.mean(correct)}')
     import ipdb; ipdb.set_trace()
