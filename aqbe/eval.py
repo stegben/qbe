520df4141a76a2be3ef6e5c886a710e22ee1fac2
stegben <stegben.benjamin@gmail.com>
2020-06-16 23:23:17 +0800

    add match_ratio evaluation

--- aqbe/eval.py
+++ aqbe/eval.py
@@ -1,3 +1,8 @@
+from typing import List
+
+import numpy as np
+
+
 def overlap_ratio(x_start, x_end, y_start, y_end):
     x_len = x_end - x_start
     y_len = y_end - y_start
@@ -5,3 +10,29 @@ def overlap_ratio(x_start, x_end, y_start, y_end):
     overlap_start = max(x_start, y_start)
     overlap_end = min(x_end, y_end)
     return max(overlap_end - overlap_start, 0.) / max(x_len, y_len)
+
+
+def match_ratio(
+        truth: str,
+        preds: List[List[str]],
+        correct_threshold: float = 0.3,
+    ):
+    scores = []
+    for pred in preds:
+        match_ratio = sum(truth == _p for _p in pred) / len(pred)
+        score = match_ratio > correct_threshold
+        scores.append(int(score))
+    return np.mean(scores)
+
+
+# TODO: standard metrics
+def OTWV():
+    pass
+
+
+def MAP():
+    pass
+
+
+def precision_at_k():
+    pass
--- aqbe/tests/test_eval.py
+++ aqbe/tests/test_eval.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aqbe.eval import overlap_ratio
+from aqbe.eval import overlap_ratio, match_ratio
 
 
 def test_overlap_ratio():
@@ -11,3 +11,7 @@ def test_overlap_ratio():
     assert overlap_ratio(0, 1, 0, 0.5) == pytest.approx(0.5/1.)
     assert overlap_ratio(0, 1, 0.5, 1) == pytest.approx(0.5/1.)
     assert overlap_ratio(0, 1, -1, 2) == pytest.approx(1./3.)
+
+
+def test_match_ratio():
+    assert match_ratio('a', [['a'], ['a', 'b'], ['a', 'b', 'c']], 0.4) == pytest.approx(2./3.)
